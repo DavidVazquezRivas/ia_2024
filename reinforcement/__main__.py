b1491612ad17d6ffab24aaa09639e3e5c9f122a3
David Vázquez Rivas <vazquezrivasdavid@gmail.com>
2024-11-24 18:07:46 +0100

    Use file to test and generate graphics

--- reinforcement/__main__.py
+++ reinforcement/__main__.py
@@ -2,28 +2,43 @@ import logging
 
 from reinforcement.agent import AgentQ
 from reinforcement.joc import Laberint
+from reinforcement.prova import Prova
 
 
 def main():
+    test = False
+
+    # Set the logging level based on the value of 'test'
+    log_level = logging.INFO if not test else logging.WARNING
     logging.basicConfig(
         format="%(levelname)-8s: %(asctime)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
+        level=log_level,
     )  # Only show messages *equal to or above* this level
 
     game = Laberint()
-    agent = AgentQ(game)
-    h, w, _ = agent.train(
-        discount=0.90,
-        exploration_rate=0.10,
-        learning_rate=0.6,
-        episodes=1000,
-        stop_at_convergence=True,
-    )
+    if (test):
+        Prova.prova(
+            game=game,
+            n=100,
+            discount=0.8,
+            exploration_rate=0.05,
+            learning_rate=0.6,
+            episodes=1000
+        )
+    else:
+        agent = AgentQ(game)
+        h, w, _ = agent.train_qlearning(
+            discount=0.90,
+            exploration_rate=0.10,
+            learning_rate=0.6,
+            episodes=1000,
+            stop_at_convergence=True,
+        )
 
-    game.reset()
-    game.set_agent([agent])
-    game.comencar()
+        game.reset()
+        game.set_agent([agent])
+        game.comencar()
 
 
 if __name__ == "__main__":
